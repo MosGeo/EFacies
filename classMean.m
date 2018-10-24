df0571c5a16dc8ac6a8584f60ef81490bf4a1408
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2018-10-24 14:25:12 -0700

    new function for analyzing results

---
+++ classMean.m
@@ -0,0 +1,19 @@
+function classMeanTable = classMean(class, data)
+%% classMean        Calculates the mean of each class for the data 
+%
+% class:                   Integer for the different classes
+% data:                    Data (each column is a variable)
+%
+% Mustafa Al Ibrahim @ 2018
+% Mustafa.Geoscientist@outlook.com
+
+%% Preprocessing
+
+
+
+%% Main
+
+
+
+
+end
