edd98431b5668d84700f10f1156ef158f3518f8f
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2018-10-26 17:24:09 -0700

    New function

---
+++ numberString.m
@@ -0,0 +1,33 @@
+function numberedStrings = numberString(prefix, numbers, delimiter)
+%% numberString   Returns a cell string array with numbered strings 
+%
+% prefix:                   Prefix string
+% numbers:                  Numbers (vector) or maximum number (scalar)
+% delimiter:                Delimiter between prefix and number
+%
+% Mustafa Al Ibrahim @ 2018
+% Mustafa.Geoscientist@outlook.com
+
+%% Preprocessing
+
+% Defaults
+if ~exist('delimiter', 'var'); delimiter = '_'; end
+
+% Assertions
+assert(exist('prefix', 'var')== true, 'prefix must be provided');
+assert(exist('numbers', 'var')== true, 'numbers must be provided');
+assert(ischar(prefix), 'prefix must be char');
+assert(isnumeric(numbers) && isvector(numbers), 'numbers must be number (vector or scalar)');
+assert(ischar(delimiter), 'delimiter must be char');
+
+%% Main
+
+if isscalar(numbers)
+   numbers = 1:numel(prefix); 
+end
+
+numbers = numbers(:);
+numberedStrings = cellfun(@(x) [prefix delimiter num2str(x)], num2cell(numbers), 'UniformOutput', false);
+
+
+end
